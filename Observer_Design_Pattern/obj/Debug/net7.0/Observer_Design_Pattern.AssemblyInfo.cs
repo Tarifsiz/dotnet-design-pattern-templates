4b836680f84cce3a59e4d963f312613a22ec5931
Ender Y <eyurdakoc@gmail.com>
2025-09-13 17:36:43 +0300

    gitignore added

--- Observer_Design_Pattern/obj/Debug/net7.0/Observer_Design_Pattern.AssemblyInfo.cs
+++ Observer_Design_Pattern/obj/Debug/net7.0/Observer_Design_Pattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Observer_Design_Pattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c75bbfb0611ec2897d9f41689ed44b3e053661ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Observer_Design_Pattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Observer_Design_Pattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
